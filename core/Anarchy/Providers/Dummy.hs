5222d65dbf4ba9e40140271a53dfe649fe7ffa6f
Estevan Castilho (Tevo) <estevan.cps@gmail.com>
2021-03-16 00:23:54 +0000

    Somebody missed the lecuture on Monads

--- core/Anarchy/Providers/Dummy.hs
+++ core/Anarchy/Providers/Dummy.hs
@@ -7,9 +7,9 @@ import Anarchy.Providers
 import Control.Monad.Trans.Maybe
 
 dummyProvider :: Champion -> Maybe Route -> MaybeT IO (Maybe Route, Rune)
-dummyProvider _ r = MaybeT . return $ Just (r, Rune { primaryStyle = 8100
-                                                    , primaryPerks = (9923, 8139, 8138, 8135)
-                                                    , secondaryStyle = 8000
-                                                    , secondaryPerks = (8009, 9103)
-                                                    , fragments = (5005, 5008, 5001)
-                                                    })
+dummyProvider _ r = return (r, Rune { primaryStyle = 8100
+                                    , primaryPerks = (9923, 8139, 8138, 8135)
+                                    , secondaryStyle = 8000
+                                    , secondaryPerks = (8009, 9103)
+                                    , fragments = (5005, 5008, 5001)
+                                    })
