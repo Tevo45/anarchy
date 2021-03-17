9b447886b2f4541e751c410678654e9553ea4d41
Estevan Castilho (Tevo) <estevan.cps@gmail.com>
2021-03-17 18:22:53 +0000

    Start work on Qt frontend

---
+++ app-qt/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+{-|
+Module      : Main
+Description : Starting point for Anarchy's Qt frontend
+Copyright   : (c) Estevan Castilho, 2021
+Maintainer  : estevan.cps@gmail.com
+Portability : Windows, POSIX
+-}
+
+main = putStrLn "hOI!"
