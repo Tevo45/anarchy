faac413471b633cd70350efb4556956ed091e888
Estevan Castilho (Tevo) <estevan.cps@gmail.com>
2021-03-18 19:00:16 +0000

    Start work on GTK+ frontend

---
+++ app-gtk/Main.hs
@@ -0,0 +1,10 @@
+{-|
+Module      : Main
+Description : Entry point for anarchy's GTK frontend
+Copyright   : (c) Estevan Castilho, 2021
+Maintainer  : estevan.cps@gmail.com
+Portability : Windows, POSIX
+-}
+module Main where
+
+main = putStrLn "hOI!"
---
+++ windows-gtk-env.el
@@ -0,0 +1,7 @@
+(setenv "PKG_CONFIG_PATH" "C:\\msys64\\mingw64\\lib\\pkgconfig")
+(setenv "XDG_DATA_DIRS" "C:\\msys64\\mingw64\\share")
+(setenv "PATH"
+	(concat
+	 "C:\\msys64\\mingw64\\bin;"
+	 "C:\\msys64\\usr\\bin;"
+	 (getenv "PATH")))
